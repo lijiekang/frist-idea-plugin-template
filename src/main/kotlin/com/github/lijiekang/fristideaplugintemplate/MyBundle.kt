2c50423ef9a6f103dd0a730f444ca6c728441695
GitHub Action <action@github.com>
2021-03-17 09:22:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/lijiekang/fristideaplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/lijiekang/fristideaplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lijiekang.fristideaplugintemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/lijiekang/fristideaplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lijiekang.fristideaplugintemplate.services
+
+import com.github.lijiekang.fristideaplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
